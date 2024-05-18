d1e3344cba3b1a709854388f0511830f3cc812ea
Heba-Webdev23 <jtvonline@yahoo.com>
2024-05-18 19:52:46 +0300

    Added create, delete and get-all

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { PrismaService } from './prisma/prisma.service';
+import { EmployeeModule } from './employee/employee.module';
 
 @Module({
-  imports: [],
+  imports: [EmployeeModule],
   controllers: [AppController],
   providers: [PrismaService],
 })
---
+++ src/employee/employee.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Post, Delete, Get } from '@nestjs/common';
+import { Employees } from '@prisma/client';
+import { Body } from '@nestjs/common';
+import { RegisterEmployee } from './create/create.dto';
+import { CreateService } from './create/create.service';
+import { DeleteEmployee } from './delete/delete.dto';
+import { DeleteService } from './delete/delete.service';
+import { GetAllService } from './get-all/get-all.service';
+
+@Controller('employee')
+export class EmployeeController {
+  constructor(
+    private readonly createService: CreateService,
+    private readonly deleteService: DeleteService,
+    private readonly getService: GetAllService,
+  ) {}
+  @Post()
+  async create(@Body() employeeData: RegisterEmployee): Promise<Employees> {
+    return this.createService.createUser(employeeData);
+  }
+
+  @Delete()
+  async delete(@Body() employeId: DeleteEmployee): Promise<Employees> {
+    return this.deleteService.deleteUser(employeId);
+  }
+
+  @Get()
+  async get() {
+    return this.getService.getUsers();
+  }
+}
