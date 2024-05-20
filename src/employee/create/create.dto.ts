2fa30e6eded3980364503d5662bd07028fe9c9e7
Heba-Webdev23 <jtvonline@yahoo.com>
2024-05-20 23:53:06 +0300

    Added class validator

--- src/employee/create/create.dto.ts
+++ src/employee/create/create.dto.ts
@@ -1,12 +1,25 @@
+import { IsEmail, Length, IsNotEmpty } from 'class-validator';
+
 export class RegisterEmployee {
-  name: string;
-  phone_number: string;
-  email: string;
-  born_date: string;
-  nationality: string;
-  national_id: string;
-  department: string;
-  job_title: string;
-  start_date: Date;
-  contract_type: string;
+  @IsNotEmpty()
+  @Length(3, 25)
+  readonly name: string;
+  @IsNotEmpty()
+  @Length(6, 25)
+  readonly phone_number: string;
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email: string;
+  @IsNotEmpty()
+  readonly born_date: string;
+  @IsNotEmpty()
+  readonly nationality: string;
+  @IsNotEmpty()
+  readonly national_id: string;
+  @IsNotEmpty()
+  readonly department: string;
+  @IsNotEmpty()
+  readonly job_title: string;
+  @IsNotEmpty()
+  readonly contract_type: string;
 }
