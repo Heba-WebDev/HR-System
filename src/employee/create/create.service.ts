70e29c1e81973184c45313886b3637ba3b975ba7
Heba-Webdev23 <jtvonline@yahoo.com>
2024-05-20 23:53:23 +0300

    Added db validation

--- src/employee/create/create.service.ts
+++ src/employee/create/create.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 import { Employees } from '@prisma/client';
 import { RegisterEmployee } from './create.dto';
@@ -8,6 +8,25 @@ export class CreateService {
   constructor(private prisma: PrismaService) {}
 
   async createUser(data: RegisterEmployee): Promise<Employees> {
+    const email = await this.prisma.employees.findFirst({
+      where: {
+        email: data.email,
+      },
+    });
+    const phone = await this.prisma.employees.findFirst({
+      where: {
+        phone_number: data.phone_number,
+      },
+    });
+    if (email) {
+      throw new HttpException('Email already exists', HttpStatus.NOT_FOUND);
+    }
+    if (phone) {
+      throw new HttpException(
+        'Phone number already exists',
+        HttpStatus.NOT_FOUND,
+      );
+    }
     return this.prisma.employees.create({
       data,
     });
